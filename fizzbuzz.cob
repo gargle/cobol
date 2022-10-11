7f694660ece92ccb9156db68035aaa28295af612
Johan Laenen <johan.laenen@bpost.be>
2022-10-11 09:14:44 +0200

    fizzbuzz, another solution for the with no advancing layout

--- fizzbuzz.cob
+++ fizzbuzz.cob
@@ -4,12 +4,16 @@
        CONFIGURATION SECTION.
        SPECIAL-NAMES.
            CONSOLE IS TERM.
+       REPOSITORY.
+           FUNCTION ALL INTRINSIC.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        77  COUNTER                                PIC 9(04).
        77  QUOTIENT                               PIC 9(03).
        77  REST3                                  PIC 9(03).
        77  REST5                                  PIC 9(03).
+       01  FIZZBUZZ-OUT                           PIC X(08).
+       01  COUNTER-OUT REDEFINES FIZZBUZZ-OUT     PIC Z(08).
        PROCEDURE DIVISION.
        0000-MAIN.
            PERFORM FIZZBUZZ VARYING COUNTER FROM 1 BY 1
@@ -19,16 +23,18 @@
        FIZZBUZZ SECTION.
            DIVIDE COUNTER BY 3 GIVING QUOTIENT REMAINDER REST3.
            DIVIDE COUNTER BY 5 GIVING QUOTIENT REMAINDER REST5.
+           MOVE COUNTER TO COUNTER-OUT.
            EVALUATE REST3 ALSO REST5
                WHEN 0 ALSO 0
-                   DISPLAY "FIZZBUZZ" WITH NO ADVANCING UPON TERM
+                   MOVE "FIZZBUZZ" TO FIZZBUZZ-OUT
                WHEN 0 ALSO NOT 0
-                   DISPLAY "FIZZ" WITH NO ADVANCING UPON TERM
+                   MOVE "FIZZ" TO FIZZBUZZ-OUT
                WHEN NOT 0 ALSO 0
-                   DISPLAY "BUZZ" WITH NO ADVANCING UPON TERM
-               WHEN NOT 0 ALSO NOT 0
-                   DISPLAY COUNTER WITH NO ADVANCING UPON TERM
+                   MOVE "BUZZ" TO FIZZBUZZ-OUT
            END-EVALUATE.
            IF COUNTER < 1000
-               DISPLAY " " WITH NO ADVANCING UPON TERM.
+               DISPLAY TRIM(FIZZBUZZ-OUT) " " WITH NO ADVANCING
+                   UPON TERM
+           ELSE
+               DISPLAY TRIM(FIZZBUZZ-OUT) UPON TERM.
            EXIT.
